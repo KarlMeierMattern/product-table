8ab82c7c9dd881c5b6fdf2aaf19c83f5733f0824
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2024-10-19 14:53:56 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,171 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+
+type Product = {
+  category: string;
+  price: string;
+  stocked: boolean;
+  name: string;
+};
+
+type Props = {
+  products: Product[];
+};
+
+type SearchProps = {
+  filterText: string;
+  onFilterTextChange: (text: string) => void;
+  inStockOnly: boolean;
+  onInstockChange: (inStockOnly: boolean) => void;
+};
+
+type ProductTableProps = {
+  products: Product[];
+  filterText: string;
+  inStockOnly: boolean;
+};
+
+function FilterableProductTable({ products }: Props) {
+  const [filterText, setFilterText] = useState<string>("");
+  const [inStockOnly, setInStockOnly] = useState<boolean>(false);
+  // state to manage text search
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <SearchBar
+        filterText={filterText}
+        onFilterTextChange={setFilterText}
+        inStockOnly={inStockOnly}
+        onInstockChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </>
   );
+}
+
+function SearchBar({
+  filterText,
+  onFilterTextChange,
+  inStockOnly,
+  onInstockChange,
+}: SearchProps) {
+  return (
+    <form>
+      <label>
+        <input
+          className="text-black"
+          type="text"
+          placeholder="...search"
+          value={filterText}
+          onChange={(e) => {
+            onFilterTextChange(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        <input
+          className="text-black"
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => {
+            onInstockChange(e.target.checked);
+          }}
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+function ProductTable({
+  products,
+  filterText,
+  inStockOnly,
+}: ProductTableProps) {
+  const [hydrated, setHydrated] = useState(false);
+
+  // Ensure client and server rendering match by waiting for hydration
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    // Render an empty table initially to avoid mismatches
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    );
+  }
+
+  const rows: JSX.Element[] = [];
+  let lastCategory = "";
+
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    } else if (inStockOnly && !product.stocked) {
+      return;
+    } else if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow product={product} key={product.category} />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="text-black pt-4">Name</th>
+          <th className="text-black pt-4">Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function ProductCategoryRow({ product }: { product: Product }) {
+  return (
+    <tr>
+      <th colSpan={2}>{product.category}</th>;
+    </tr>
+  );
+}
+
+function ProductRow({ product }: { product: Product }) {
+  const displayName = product.stocked ? "text-black" : "text-red-500";
+
+  return (
+    <tr>
+      <td className={`${displayName} pr-8`}>{product.name}</td>
+      <td className={displayName}>{product.price}</td>
+    </tr>
+  );
+}
+
+const products = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default function App() {
+  return <FilterableProductTable products={products} />;
 }
